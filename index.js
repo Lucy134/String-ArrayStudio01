c41844f8e62ef08eaf5f3648ac44b93e4bb36dd6
Lucy <>
2021-05-25 00:45:56 +0000

    More unnecessary variable removal

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ if(x > str.length){
   snark = "I'm sure you thought you were being very clever putting in a number bigger than the word, too bad I changed your number.";
 }
 
-
-let newStr = str.substring(x,str.length);
-let str1 = newStr.concat(str.substring(0,x));
+let str1 = str.substring(x,str.length).concat(str.substring(0,x));
 //a) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
@@ -17,9 +15,6 @@ let str1 = newStr.concat(str.substring(0,x));
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
 console.log(`Original: ${str}, modified: ${str1} \n${snark}`)
-//console.log(sStr);
-//console.log(newStr);
-//onsole.log(str1);
 
 //b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
